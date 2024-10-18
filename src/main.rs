f5a037abb95e1692016d43fff101ae30792be78d
Jason Ribble <jasonribble@protonmail.com>
2024-10-18 12:46:03 -0700

    ci(fix): run api in action
    seems like a quick way to run integration tests

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn main() {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8065")
         .await
         .unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
+    println!("API: listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
