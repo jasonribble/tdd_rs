0976d48f38924668b000ffdc92dfdd64936cbb54
Jason Ribble <jasonribble@protonmail.com>
2024-10-18 10:50:14 -0700

    feat: ok api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/ok", get(handler));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8065")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> &'static str {
+    "ok"
 }
